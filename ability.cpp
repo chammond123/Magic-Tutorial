cae162dfe33cec5d73da99bb76a518ee3e3ef035
SimonVo05 <voquocminhh@gmail.com>
2025-04-07 21:16:09 -0600

    Implement ability class, would add a library of ability

--- ability.cpp
+++ ability.cpp
@@ -1,3 +1,14 @@
 #include "ability.h"
 
-Ability::Ability() {}
+Ability::Ability(QString name, QString description, std::function<void(Player*, Card*)> eff){
+    this->name = name;
+    stringEffect = description;
+    effect = eff;
+}
+
+// Activate method
+void Ability::activate(Player* player, Card* target){
+    if (effect) {
+        effect(player, target);
+    }
+}
--- ability.h
+++ ability.h
@@ -1,10 +1,23 @@
 #ifndef ABILITY_H
 #define ABILITY_H
 
+#include <QString>
+
+class Player;
+class Card;
+
 class Ability
 {
+
+private:
+    QString name;
+    QString stringEffect;
+    std::function<void(Player* player, Card* card)> effect;
+
+
 public:
-    Ability();
+    Ability(QString name, QString description, std::function<void(Player*, Card*)> eff);
+    void activate(Player* player, Card* target);
 };
 
 #endif // ABILITY_H
