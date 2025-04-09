520192fd98c0a3d77a0197a070993b54b83ba736
Chase Hammond <chasedhammond@gmail.com>
2025-04-09 17:18:53 -0600

    added Header for the phase enum and struct.

--- phase.h
+++ phase.h
@@ -1,4 +1,25 @@
 #ifndef PHASE_H
 #define PHASE_H
-
+enum class Phase {
+    Untap,
+    Upkeep,
+    Draw,
+    PreCombatMain,
+    BeginCombat,
+    DeclareAttackers,
+    DeclareBlockers,
+    CombatDamage,
+    PostCombatMain,
+    EndStep,
+    Cleanup
+};
+struct PhaseRules {
+    bool canPlayInstant;
+    bool canPlaySorcery;
+    bool canDraw;
+    bool canDeclareAttack;
+    bool canDeclareDefense;
+    bool canPassTurn;
+    bool canUntap;
+};
 #endif // PHASE_H
