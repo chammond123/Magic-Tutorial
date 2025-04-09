d31463a0516067c2d3171686f577d13fcab49ff9
SimonVo05 <voquocminhh@gmail.com>
2025-04-09 14:13:59 -0600

    Added ability class

--- ability.h
+++ ability.h
@@ -10,14 +10,22 @@ class Ability
 {
 
 private:
-    QString name;
-    QString stringEffect;
+    // QString name;
+    // QString stringEffect;
     std::function<void(Player* player, Card* card)> effect;
 
 
 public:
-    Ability(QString name, QString description, std::function<void(Player*, Card*)> eff);
+    Ability(std::function<void(Player*, Card*)> eff);
     void activate(Player* player, Card* target);
+
+    static Ability damageCreature(int amount);
+    static Ability damagePlayer(int amount);
+    static Ability buff(int amount);
+    static Ability heal(int amount);
+    static Ability addMana(int amount);
+    static Ability destroy();
+
 };
 
 #endif // ABILITY_H
