f5ea7f387201a44edbbeb711650c98373063ffc3
Zach Royer <zach.royer14@gmail.com>
2025-04-17 14:44:43 -0600

    Initial Scene crafting in Main.cpp
    Connecting a deck list file to the API.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,36 @@
 #include "mainwindow.h"
 #include "player.h"
+#include "textparser.h"
+#include "carddictionary.h"
+#include "card.h"
+#include "cardapimanager.h"
 
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    /**
+     * Test Phases / Game States
+     */
+
+    CardAPIManager *apiManager = new CardAPIManager();
+
+    // Connecting the API Manager's signal
+    QObject::connect(apiManager, &CardAPIManager::cardFetched, [=](const Card &card) {
+        cardDictionary::addCard(card);
+        qDebug() << "Static Card Dictionary updated after a card was fetched";
+    });
+
+    // Instantiating a deck list
+    QStringList deckList = TextParser::getListFromText(QFile(":/text/additional_files/deck.txt"));
+
+    // Fetching from the API
+    apiManager->fetchCardsByNames(deckList);
+
+    // Player userPlayer()
+
     MainWindow w(nullptr);
     w.show();
     return a.exec();
